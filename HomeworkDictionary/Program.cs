05f703855fd19f3a797d20117e5c3f296bc1f41e
Taiwei Ko <152499488+macgeek39@users.noreply.github.com>
2024-01-07 07:45:54 -0600

    Initialized Commit

--- HomeworkDictionary/Program.cs
+++ HomeworkDictionary/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+/*
+ * Create a Dictionary list of employee IDs
+ * and the name that goes with the ID.
+ * Fill in a few records. Then ask the user for their ID
+ * and return their name
+ */
+
